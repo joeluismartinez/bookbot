f7bee985593b61cf4f1acbebd9f82c65ddbba2a5
joeluismartinez <wbprgrmr86@gmail.com>
2024-04-21 20:53:31 -0500

    working

--- main.py
+++ main.py
@@ -2,19 +2,32 @@
 
 def main():
     letter_dict = dict()
+    letter_list = []
+
     with open("frankenstein.txt") as f:
         file_contents = f.read()
 
-    print(len(file_contents.split()))
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(file_contents.split())} words found in the document\n")
     count_letters(file_contents, letter_dict)
-    print(letter_dict)
+    for item in letter_dict:
+        letter_list.append({"name": item, "num": letter_dict[item]})
+    letter_list.sort(reverse=True, key=sort_on)
+    for item in letter_list:
+       print(f"The '{item['name']}' character was found {item['num']} times")
+    print("--- End report ---")
+
 
 def count_letters(file_contents, letter_dict):
     for word in file_contents.split():
         for letter in word:
             key = letter.lower()
-            if key not in letter_dict:
-                letter_dict[key] = 1 
+            if key not in letter_dict and key.isalpha():
+                letter_dict[key] = 0
             if key in letter_dict:
                 letter_dict[key] += 1
+
+def sort_on(dict):
+    return dict["num"]
+
 main()
